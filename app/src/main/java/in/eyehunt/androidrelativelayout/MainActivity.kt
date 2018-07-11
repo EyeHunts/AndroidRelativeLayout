b5d2be9d59947ba81ad9d8316b99c66af734a912
Rohit <rkkanojia7@gmail,com>
2018-07-11 11:39:23 +0530

    Initial commit

---
+++ app/src/main/java/in/eyehunt/androidrelativelayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package `in`.eyehunt.androidrelativelayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
